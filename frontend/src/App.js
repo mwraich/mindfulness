631d03b22c77d995d4eda157a2835eb9877cc639
Reggie <ryoboua@gmail.com>
2018-11-17 21:49:09 -0500

    Added homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Box, Button } from 'grommet';
+import { User, UserSettings } from 'grommet-icons';
+
+import logo from './logo.jpg'
+
+const theme = {
+  global: {
+    colors: {
+      brand: 'white'
+    },
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+    <Box
+      tag='header'
+      direction='row'
+      align='center'
+      justify='between'
+      background='brand'
+      pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+      style={{ zIndex: '100' }}
+      {...props}
+    />
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Grommet theme={theme} full>
+      <Box fill>
+          <AppBar>
+            <img src={logo} width="150" height="90" />
+          <Button icon={<User />}/>
+          </AppBar>
+
+        <Box
+          height="small"
+          width="100%"
+          onClick={() => console.log('yo')}
+          margin={{
+            bottom: '75px',
+            top: '50px'
+          }}
+
+        >
+        <Button
+          primary
+          color="neutral-4"
+          label="Create Session"
+          fill
+          style={{
+            width: '90%',
+            color: 'white'
+          }}
+        />
+        </Box>
+        <Box
+          alignSelf="end"
+          background="white"
+          height="small"
+          width="100%"
+          onClick={() => console.log('yo')}
+
+        >
+        <Button
+          primarys
+          alignSelf="end"
+          label="Join Session"
+          fill
+          style={{
+            background: '#00C781',
+            width: '90%',
+            color: 'white'
+          }}
+        />
+        </Box>
+      </Box>
+      </Grommet>
+    )
   }
 }
 
 export default App;
+
