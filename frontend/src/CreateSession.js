05cffce9ab7c2d4a3ed4d65132597375e6ca26e1
Mandeep Wraich <mandeepw@nulogy.com>
2018-11-17 23:02:54 -0500

    Add button components for create screen

--- frontend/src/CreateSession.js
+++ frontend/src/CreateSession.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
-
+import { Grommet, Box, Button } from 'grommet';
+import CategoryButton from './CategoryButtons';
+const theme = {
+  global: {
+    colors: {
+      brand: '#00C781'
+    },
+    font: {
+      family: 'Roboto',
+      size: '20px'
+    },
+  },
+};
 
 class CreateSession extends Component {
     render(){
-        return (
-            <div>
-                we up
-            </div>
+      return(
+            <CategoryButton/>
         )
     }
 }
