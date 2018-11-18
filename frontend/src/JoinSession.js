ec42126b15273fdec775654402c4c7df2e6103de
Mandeep Wraich <mandeepw@nulogy.com>
2018-11-18 01:10:26 -0500

    Add image for join page

--- frontend/src/JoinSession.js
+++ frontend/src/JoinSession.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import { Grommet, Box, Button } from 'grommet';
+import ActivityPage from './ActivityPage.png'
 
+const theme = {
+  global: {
+    colors: {
+      brand: '#00C781'
+    },
+    font: {
+      family: 'Poppins',
+      size: '20px'
+    },
+  },
+};
 
 class JoinSession extends Component {
     render(){
         return (
-            <div>
-                we up
-            </div>
+              <Grommet theme={theme} full>
+                <img src={ActivityPage} alt="User"/>
+                </Grommet>
         )
     }
 }
